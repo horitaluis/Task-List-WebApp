c8c838ab614b529f3388fe7598eba737b97a4b4b
Luis Horita <horitaluis@gmail.com>
2019-02-04 17:13:08 -0200

    favorite arg to createNewTask
    adding the favorite argument to createNewTask function and adding the class favorite-img-checked is the favorite is == true and removing if dont.

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,7 @@
 console.log("teste");
 
 // function que cria uma nova task a partir dos dados do form
-function createNewTask(day, icon, name, description, identification) {
+function createNewTask(day, icon, name, description, favorite, identification) {
 
 	//cria os elementos da nova task e coloca seus atributos neles
 	var newTask = document.createElement("div");
@@ -103,6 +103,12 @@ function createNewTask(day, icon, name, description, identification) {
 
 	var newFavoritePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
 	newFavoritePath.setAttribute("d", "M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z");
+
+	if (favorite == true) {
+		newFavoriteImg.classList.add("favorite-img-checked");
+	} else {
+		newFavoriteImg.classList.remove("favorite-img-checked");
+	}
 
 	newFavoriteImg.addEventListener("click", function(event) {
 		event.preventDefault();
